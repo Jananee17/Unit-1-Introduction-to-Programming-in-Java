4c5262601152bdc3a763d245599836c56bebdeb1
Jananee <119385174+Jananee17@users.noreply.github.com>
2023-01-05 16:59:19 -0500

    L1-2 Try It Q3 Variables
    This is my answer to try it question 3 lesson 1-2 in unit 1.

---
+++ L1-2 Try It Q3 Variables - Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+  /* LastChoice, two times and true are incorrect and they do not follow Java style conventions. 
+  * Since first one both words are capitalized first letter, two times wrong since both are 
+  * lowercase first letter and true is boolean, not a variable word that can be used. 
+  * The ones that would give compiler errors are _system, $1 and third-choice are not 
+  * allowed because first one _ is allowed bat can’t start in beginning with _, second 
+  * one $1 is wrong will give error since you can’t start it with $ and third-choice is 
+  * wrong since your not allowed to use - for variable name.
+  */
+}
