76934e6a86c2d9037e2ba953121b9bc4afb6a534
Jananee <119385174+Jananee17@users.noreply.github.com>
2023-01-05 16:55:17 -0500

    L1-2 Try It Q1 Variables
    This is my answer to try it question 1 in lesson 2-1 Variables in unit 1.

---
+++ L1-2 Try It Q1 Variables - Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+  /* In Java it is a statically typed language meaning you need to define a name 
+  * and the kind of specific value for the variable such as integer (int), 
+  * boolean value- (boolean) or a real number so Java stores it as a variable. 
+  * Then after a variable is declared in Java the name and type can’t be changed, 
+  * but the value stored in the variable can be changed while the program runs. 
+  * However, in Python you don’t really need to declare a variable since it is 
+  * dynamically typed meaning they can store any type of data while the program runs. 
+  * That's the difference between declaring variables in Java vs Python.
+}
