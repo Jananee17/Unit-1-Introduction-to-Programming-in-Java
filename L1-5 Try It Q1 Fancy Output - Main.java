3060a60d5ca843fa540fc63c61e478da3c6eefa2
Jananee <119385174+Jananee17@users.noreply.github.com>
2023-01-05 17:10:44 -0500

    L1-5 Try It Q1 Fancy Output
    This is my answer to try it Q1 in lesson 1-5 Fancy output in unit 1.

---
+++ L1-5 Try It Q1 Fancy Output - Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+  /* The formatting specifier is what changes to the position of where the 
+  * type of data would be printed when it's in code in a Java program 
+  * using the formatting specifier (the % sign).
+  */
+}
+  
