251b65df30061a62f5e5720af4c0772cc32a1025
Jananee <119385174+Jananee17@users.noreply.github.com>
2023-01-05 17:21:17 -0500

    L1-9 Try It Q1 While Loop
    This is my answer for question 1 try it in lesson 1-9 While Loop in unit 1.

---
+++ L1-9 Try It Q1 While Loop - Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+   public static void main (String[] args)
+   {
+       while(true)
+       {
+           for(int sec = 10; sec > 0; sec--)
+           {
+               System.out.println(sec + " second(s) to go.");
+           }
+       }
+   }
+}
