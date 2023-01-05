d998867e672cf6908f2aff2295aeb80ad1e3dc6a
Jananee <119385174+Jananee17@users.noreply.github.com>
2023-01-05 16:57:23 -0500

    L1-2 Try It Q2 Variables
    This is my answer to try it question 2 lesson 1-2 in unit 1.

---
+++ L1-2 Try It Q2 Variables - Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+  /* If you don’t declare the variable and just put the initial variable setting the 
+  * initial variable Java will give you an error saying symbol: variable age cannot 
+  * be found. If the variable is declared and then the initial variable is put 
+  * then it would work with no errors.
+  /*
+}
