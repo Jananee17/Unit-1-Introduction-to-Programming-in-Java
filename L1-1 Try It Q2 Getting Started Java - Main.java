5683e2a702ba9f2b72d7836d1cf9483ba31f9fd2
Jananee <119385174+Jananee17@users.noreply.github.com>
2023-01-05 16:48:21 -0500

    L1-1 Try It Q2 Getting Started Java
    This is my answer to question 2 try it in Lesson 1-1 and unit 1.

---
+++ L1-1 Try It Q2 Getting Started Java - Main.java
@@ -0,0 +1,7 @@
+public class Main
+{
+  /* Java’s main weakness is it is slow and has poor performance 
+  * because it is memory consuming. Also, it is slower than other programming languages 
+  * such as C++ since each code has to be interpreted to the machine level code.
+  */
+}
