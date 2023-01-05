87215bf8920e939f11a2cbe9a8445de29e34a911
Jananee <119385174+Jananee17@users.noreply.github.com>
2023-01-05 17:14:10 -0500

    L1-7 Try It Q1 If Statements
    This is my answer to question 1 try it in lesson 1-7 If statements in unit 1.

---
+++ L1-7 Try It Q1 If Statements - Main.java
@@ -0,0 +1,28 @@
+public class Main
+{
+   public static void main(String[] args)
+   {
+       String p = "p";
+       String q = "q";
+       String j = "j";
+       int i = 1;
+       int a = 0;
+
+       if (true);
+       {
+           System.out.println(p);
+           System.out.println(q);
+           System.out.println(j);
+       }
+
+       if (true)
+       {
+           System.out.println("First integer is: " + i);
+           System.out.println("Second integer is: " + a);
+       }
+       else
+       {
+           System.out.println("Thats not an integer.");
+       }
+   }
+}
