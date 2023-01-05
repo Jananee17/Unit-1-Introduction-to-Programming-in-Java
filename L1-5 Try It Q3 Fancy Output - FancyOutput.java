302b946eade15bc1345880ac9c55a3563d72c38b
Jananee <119385174+Jananee17@users.noreply.github.com>
2023-01-05 17:12:17 -0500

    L1-5 Try It Q3 Fancy Output
    This is my answer to question 3 try it lesson 1-5 in unit 1.

---
+++ L1-5 Try It Q3 Fancy Output - FancyOutput.java
@@ -0,0 +1,11 @@
+public class FancyOutput
+{
+   public static void main(String[] args)
+   {
+       System.out.printf("%.3s \n", "*");
+       System.out.printf("%.3s \n", "**");
+       System.out.println("*  *");
+       System.out.printf("%.3s \n", "**");
+       System.out.printf("%.3s \n", "*");
+   }
+}
