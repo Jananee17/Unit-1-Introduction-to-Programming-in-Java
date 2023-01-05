3f28c8038d8b059c2729738c0939e5d91c8b1c9b
Jananee <119385174+Jananee17@users.noreply.github.com>
2023-01-05 16:45:28 -0500

    L1-1 Try It Q1 Getting Started Java
    This is my answer to question 1 try it in Lesson 1-1 Getting Started Java in Unit 1.

---
+++ L1-1 Try It Q1 Getting Started Java - Main.java
@@ -0,0 +1,7 @@
+public class Main
+{
+  */ Another strength of Java is it allows you to create object oriented programs, which allow you to create modular programs and resume code. 
+  * The second other strength of Java is a programming language that is easy to use, create, debug and compile programs. 
+  * Third strength of Java is that it is easy and simple to learn.
+  /*
+}
