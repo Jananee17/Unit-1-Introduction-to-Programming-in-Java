ea14ab3c7080217a70cc543ecb9669013d401f9b
Jananee <119385174+Jananee17@users.noreply.github.com>
2023-01-05 17:03:56 -0500

    L1-4 Try It Q1 Getting User Input
    This is my answer to try it question 1 lesson 4-1 Getting User Input and in unit 1.

---
+++ L1-4 Try It Q1 Getting User Input - Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+  /* The difference between the three types of errors are that a logical error occurs 
+  * when your code compiles but does not give the result you expect. For example 
+  * in Java if you the quotient of 3 / 5 would be 0 (not 0.6) and the remainder 3. 
+  * If you misspell any code or words when adding code Java can detect it and that 
+  * is the error that would come up called syntax error and IDE’s will highlight 
+  * the error of syntax in color. For example, if your program in Java had variable 
+  * int age = 17.5 created (in Java can’t convert 17.5 to int and it is syntax error 
+  * as you can’t lose fractional part. Whereas the run time error is different from 
+  * logic and syntax error, is a type of exception meaning the program encounters 
+  * unexpected input that is unable to deal with it. For example nextInt() 
+  * method is only used to read and return an integer only, if you enter other 
+  * types of data the program will say run time error and crash.
+  */
+}
