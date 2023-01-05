e6fff0fe928700d0bb2ecbd07c744e2d4ef7f28f
Jananee <119385174+Jananee17@users.noreply.github.com>
2023-01-05 16:50:43 -0500

    L1-1 Try It Q3 Getting Started Java
    This is my answer to try it question 3 lesson 1-1 in unit 1.

---
+++ L1-1 Try It Q3 Getting Started Java - Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+  /* If you spell for example print wrong in println code instead it says 
+  * System.out.pirntln() Java will say it's wrong in red. It says its error because the 
+  * symbol cannot be found since print is spelled wrong. Another error could 
+  * be you forgot to add the semicolon ----> ;  after the new line of code 
+  * and in Java it detects an error in red saying expected ;. 
+  * Meaning you needed to add a semicolon. Then where it needs to be added 
+  * there is a red squiggly line on the line where the semicolon is missing.
+  */
+}
+
