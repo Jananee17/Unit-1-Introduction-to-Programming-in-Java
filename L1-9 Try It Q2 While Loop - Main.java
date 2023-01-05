094d66174381c85ce3622026d0b241870bf68763
Jananee <119385174+Jananee17@users.noreply.github.com>
2023-01-05 17:22:23 -0500

    L1-9 Try It Q2 While Loop
    This is my answer for question 2 try it lesson 1-9 in unit 1.

---
+++ L1-9 Try It Q2 While Loop - Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+   public static void main(String[] args)
+   {
+       for(int count=0; count < 20; count++)
+       {
+           System.out.printf("count is %d.\n", count);
+       }
+
+   }
+}
